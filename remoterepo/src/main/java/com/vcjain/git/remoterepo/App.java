6566f49a372eacb4c4d4a1528d1a1176316621d5
vcjain <vikashjain@hotmail.com>
2013-04-04 17:32:34 +0530

    Commit: 2

--- remoterepo/src/main/java/com/vcjain/git/remoterepo/App.java
+++ remoterepo/src/main/java/com/vcjain/git/remoterepo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Commit in Master :1" );
+        System.out.println( "Commit in Master :2" );
     }
 }
